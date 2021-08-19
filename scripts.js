f54e960647862d9557a8eed128a8544d2f2c9e16
vananz123 <88036322+vananz123@users.noreply.github.com>
2021-08-19 16:54:17 +0700

    addd

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
   $('.btn').click(function(){
     $(this).toggleClass("click");
@@ -9,22 +10,21 @@ $(document).ready(function(){
   });
   $(window).scroll(function(){
     if($(window).scrollTop() >=120){
-      $('.gotop span').css({
-        "display":"block"
-      })
+      $('.gotop span').show("slow")
     }else{
-      $('.gotop span').css({
-        "display":"none"
-      })
+      $('.gotop span').hide("slow")
     }
   })
   $(window).scroll(function(){
     if($(window).scrollTop()>100){
       $('div.headerr').css({
+        "position": "fixed",
+        "z-index":"1",
         "background": "rgba(0, 0 ,0,0.5)"
       })
     }else{
       $('div.headerr').css({
+        "position":"absolute",
         "background": "none"
       })
     }
@@ -34,4 +34,22 @@ $(document).ready(function(){
       scrollTop:0
     },2000)
   })    
+
+  var od =document.querySelectorAll('div.orderonl img')
+  for(var i=0; i<od.length;i++){
+    var aa=od[i];
+    $(aa).on({
+      mouseenter: function() {
+          $(this).css({
+              "border-radius":"40px",
+              "background-color": "gold"
+          })
+      },
+      mouseleave: function() {
+          $(this).css({
+              "background-color":"white"
+          })
+      }
+  })
+  }
 });
