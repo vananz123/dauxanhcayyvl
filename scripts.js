a2423c11fb5fa7ba6d9dfadf7dfeaf22d437f5f7
vananz123 <88036322+vananz123@users.noreply.github.com>
2021-09-02 19:51:05 +0700

    sddfdf

--- scripts.js
+++ scripts.js
@@ -23,16 +23,6 @@ $(document).ready(function(){
       scrollTop:0
     },1800)
   })
-  function header_ef(){
-    $(window).scroll(function(){
-      if($(window).scrollTop() > 10){
-        $('div.headerr').addClass("skit")
-      }else{
-        $('div.headerr').removeClass("skit")
-      }
-    })
-  }
-  header_ef();
   // $(window).scroll(function(){
   //   if($(window).scrollTop() >=2336){
   //     $('body').css("background-image","url('https://c.pxhere.com/photos/e3/78/mug_coffee_cup_drink-98978.jpg!d')")
@@ -232,17 +222,68 @@ $(document).ready(function(){
     })
   }
   sendsmit();
+  const widthHeaderr=65;
+  function showHeaderM(){
+    var heightheader=document.querySelector('div.headerr.skit')
+    var headerrSkitNavv=document.querySelector('div.headerr.skit .navv')
+    var headerrSkitlogo=document.querySelector('div.headerr.skit #logo')
+    function navvshow(obj1,obj2,obj3){
+      $(obj1).fadeToggle();
+      $(obj2).fadeToggle("slow");
+      $(obj3).fadeToggle(3000);
+    }
+    $(window).scroll(function(){
+      if($(window).scrollTop() > 5){
+        $(heightheader).css({
+          "height":`${widthHeaderr}px`
+        })
+        $(headerrSkitNavv).css({
+          "right":"0"
+        })
+        $(headerrSkitlogo).css({
+          "top":"10px",
+          "left":"10px",
+          "width":"50px",
+          "opacity":"1",
+          "transform":"translateX(0)"
+        })
+        $('div.headerr').addClass("skit")
+      }else{
+        $(heightheader).css({
+          "height":"100vh"
+        })
+        $(headerrSkitNavv).css({
+          "right":"-150%"
+        })
+        $(headerrSkitlogo).css({
+          "top":"15%",
+          "left":"50%",
+          "width":"400px",
+          "opacity":"0.5",
+          "transform":"translateX(-50%)"
+        })
+        $('div.headerr').removeClass("skit")
+      }
+    })
+    $('.navbar .btn').click(function(){
+      $('div.headerr').toggleClass("show_header_m")
+    })
+  }
+  showHeaderM();
+
   function nav_ani(){
     var ani =document.querySelector('.nav_list .ani.move')
-    var nis =document.querySelectorAll('.nav_list li')
+    var nis =document.querySelectorAll('.nav_list > li')
     function sett(j){
       const m=500*1/5
+      var kt;
       var p=`${m}px`
       var left=m*(j)
       $(ani).css({
         "width":p,
         "left":left
-      })
+      })     
+
     }
     for(let j=0;j<nis.length;j++){
       nis[j].addEventListener('mouseover',function(){
@@ -251,12 +292,53 @@ $(document).ready(function(){
     }
   }
   nav_ani();
-  function showHeaderM(){
-    $('.navbar .btn').click(function(){
-      $('div.headerr').toggleClass("show_header_m")
-    })
+  function headerMeun(){
+    var nis =document.querySelectorAll('.nav_list > li')
+    var listProfile=document.querySelector('.nav_list li .list_hide_profile')
+    var listMenu=document.querySelector('.nav_list li .list_hide_meun')
+    var heightheader=document.querySelectorAll('div.headerr.skit')
+    //hàm ẩn đi list menu
+    function headerrHide65(obj){
+      $(obj).css("display","none")
+      $(heightheader).css({
+        "height":`${widthHeaderr}px`
+      })
+      $('div.headerr').addClass("skit")
+    }
+    //hàm hiện list menu
+    function headerrShowe65(obj,height){
+      $(obj).css("display","block")
+      $(heightheader).css({
+        "height":`${height+widthHeaderr}px`
+      })
+      $('div.headerr').addClass("skit")
+    }
+    function setenter(j){
+      if(j==1){
+        headerrShowe65(listProfile,$(listProfile).height())
+      }
+      if(j==2){
+        headerrShowe65(listMenu,$(listMenu).height())
+      }
+    }
+    function setleave(j){
+      if(j==1){
+        headerrHide65(listProfile)
+      }
+      if(j==2){
+        headerrHide65(listMenu)
+      }
+    }
+    for(let j=0;j<nis.length;j++){
+      nis[j].addEventListener('mouseenter',function(){
+        setenter(j)
+      })
+      nis[j].addEventListener('mouseleave',function(){
+        setleave(j)
+      })
+    }
   }
-  showHeaderM();
+  headerMeun();
   function slider_mobile(){
     var slider=document.querySelectorAll('.sliders .tt')
     var dot=document.querySelectorAll('.dot .dots')
